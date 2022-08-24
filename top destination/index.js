ebb17d6d1f43bc3145ca1c4a5ab0f04f75288a7f
soumitra-dey <soumitra.petbindhi@gmail.com>
2022-08-24 23:06:36 +0530

    (day-2)top destination-design complete, data pending

---
+++ top destination/index.js
@@ -0,0 +1,19 @@
+
+let citybtn=document.querySelector("#citytype_city");
+let typesbtn=document.querySelector("#citytype_types");
+let addlist=document.querySelector(".addlist")
+let typeslist=document.querySelector(".typeslist")
+
+
+citybtn.addEventListener("click", ()=>{
+    citybtn.style.borderBottom="1px solid blueviolet";
+    typesbtn.style.borderBottom="0px";
+    addlist.classList.remove("flag")
+    typeslist.classList.add("flag")
+})
+typesbtn.addEventListener("click", ()=>{
+    citybtn.style.borderBottom="0px"
+    typesbtn.style.borderBottom="1px solid blueviolet"
+    typeslist.classList.remove("flag")
+    addlist.classList.add("flag")
+})
