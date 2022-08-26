7416053065f57b324aa41c228e4019dbb8530edd
Honey <honeysnh7861@gmail.com>
2022-08-26 19:25:19 +0530

    footer

---
+++ component/NorthAmerica.js
@@ -0,0 +1,61 @@
+function NorthAmerica(){
+    return`     <div>
+        <p>United States Of America</p>
+        <p>3017 holiday homes</p>
+        </div>
+        <div>
+        <p>Mexico</p>
+        <p>569 holiday homes</p>
+        </div>
+        <div>
+        <p>Dominican Republic</p>
+        <p>365 holiday homes</p>
+        </div>
+        <div
+        <p>Cuba</p>
+        <p>257 holiday homes</p>
+        </div>
+        <div>
+        <p>Canada</p>
+        <p>164 holiday homes</p>
+        </div>
+        <div>
+        <p>Costa Rica</p>
+        <p>147 holiday homes</p>
+        </div>
+        <div>
+        <p>Jamaica</p>
+        <p>115 holiday homes</p>
+        </div>
+        <div>
+        <p>Aruba</p>
+        <p>37 holiday homes</p>
+        </div>
+        <div>
+        <p>Puerto Rico</p>
+        <p>37 holiday homes</p>
+        </div>
+        <div>
+        <p>Saint Martin</p>
+        <p>34 holiday homes</p>
+        </div>
+        <div>
+        <p>Sint Maarten</p>
+        <p>10 holiday homes</p>
+        </div>
+        <div>
+        <p>Belize</p>
+        <p>9 holiday homes</p>
+        </div>
+        <div>
+        <p>Panama</p>
+        <p>9 holiday homes</p>
+        </div>
+        <div>
+        <p>Curacao</p>
+        <p>6 holiday homes</p>
+        </div>
+        `
+}
+
+export default NorthAmerica;
